23172ef774fe77604ca48ebe63c8ad533b2f70f4
Dawson June <dawsonjjune@gmail.com>
2020-12-05 05:27:07 -0800

    Created and began work on JS functionality

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+//** Variables **//
+
+// Decks //
+
+let sheetDeck = document.querySelector(".sheetDeck");
+let noteDeck = document.querySelector(".noteDeck");
+let fingerDeck = document.querySelector(".fingerDeck");
+
+// Question Selectors //
+
+let noteQuestion = document.querySelector(".note-music");
+let nameQuestion = document.querySelector(".note-name");
+let fingerQuestion = document.querySelector(".note-finger");
+
+// Answer Selectors //
+
+let answerOne = document.querySelector(".quiz-area__answer-1");
+let answerTwo = document.querySelector(".quiz-area__answer-2");
+let answerThree = document.querySelector(".quiz-area__answer-3");
+let answerFour = document.querySelector(".quiz-area__answer-4");
+
+// Button Selectors //
+
+let submitBtn = document.querySelector("btn__submit");
+
+//////////////////////////////////////////////////////////////////
+
