0335d7bd91171caf21fb79e5d9315364b25cd7f4
Dawson June <dawsonjjune@gmail.com>
2020-12-12 03:50:29 -0800

    Major Update
    Began to construct flashcard logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,85 @@ let answerFour = document.querySelector(".quiz-area__answer-4");
 
 // Button Selectors //
 
-let submitBtn = document.querySelector("btn__submit");
+let submitBtn = document.querySelector(".btn__submit");
+
+// Image Selector //
+
+let quizImage = document.querySelector(".image-holder");
 
 //////////////////////////////////////////////////////////////////
+
+// Sheet Music Deck //
+
+let deckSheetMusic = {
+    "card" : [
+        {
+            "note" : "B Flat",
+            "imgSrc" : "../../build/notes/bflat-1.png",
+            // Answers //
+            "correct" : "B Flat",
+            "incorrect-1" : "B",
+            "incorrect-2" : "C Flat",
+            "incorrect-3" : "G"    
+        },
+        {
+            "note" : "B",
+            "imgSrc" : "../../build/notes/b-1.png",
+            // Answers //
+            "correct" : "B",
+            "incorrect-1" : "C",
+            "incorrect-2" : "E Flat",
+            "incorrect-3" : "D"    
+        },
+        {
+            "note" : "C",
+            "imgSrc" : "../../build/notes/c-1.png",
+            // Answers //
+            "correct" : "C",
+            "incorrect-1" : "B",
+            "incorrect-2" : "E",
+            "incorrect-3" : "A"    
+        },
+        {
+            "note" : "C Sharp",
+            "imgSrc" : "../../build/notes/csharp-1.png",
+            // Answers //
+            "correct" : "C Sharp",
+            "incorrect-1" : "D Sharp",
+            "incorrect-2" : "C Flat",
+            "incorrect-3" : "B Sharp"    
+        },
+        {
+            "note" : "D",
+            "imgSrc" : "../../build/notes/d-1.png",
+            // Answers //
+            "correct" : "D",
+            "incorrect-1" : "C",
+            "incorrect-2" : "A",
+            "incorrect-3" : "B"    
+        },
+        {
+            "note" : "D Sharp",
+            "imgSrc" : "../../build/notes/dsharp-1.png",
+            // Answers //
+            "correct" : "D Sharp",
+            "incorrect-1" : "C Sharp",
+            "incorrect-2" : "E Flat",
+            "incorrect-3" : "A Flat Sharp"    
+        }
+    ]
+}
+
+// Note Names Deck //
+
+
+function testing () {
+    quizImage.innerHTML = "<img src=" + deckSheetMusic.card[0].imgSrc + ">";
+}
+
+testing();
+
+
+
+
 
